2cadfdc1fd06189960f8e5aeef3a7206f5f281b3
igi712 <igi712.mgj@gmail.com>
2025-11-03 18:34:01 +0700

    feat: about us pop up

---
+++ pcd_try-on-ui-godot/popup.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_info_button_pressed():
+	$"../Popup".visible = true # Replace with function body.
+
+
+func _on_exit_button_pressed():
+	$"../Popup".visible = false # Replace with function body.
